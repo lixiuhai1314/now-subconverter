02858163ac11b739cbebb19ce3e24bde48a78979
GitHub Action <noreply@github.com>
2022-05-27 20:19:04 +0000

    Update sub-web from zhongfly/sub-web

---
+++ sub-web/js/precache-manifest.fc579fabc77fff6760ddebb692b19517.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72cd7d7b6923016ad755aed91ed7ed31",
+    "url": "_redirects"
+  },
+  {
+    "revision": "e6b95f2d9356b852afaf",
+    "url": "css/app.6a5dab7f.css"
+  },
+  {
+    "revision": "e40946e79bd24847f01d",
+    "url": "css/chunk-vendors.53794358.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "b47137f5b3d471ccd083df435c641b2a",
+    "url": "index.html"
+  },
+  {
+    "revision": "e6b95f2d9356b852afaf",
+    "url": "js/app.7cd8d81f.js"
+  },
+  {
+    "revision": "92041e42a5d9227a2386",
+    "url": "js/chunk-87bd93be.3b08d50b.js"
+  },
+  {
+    "revision": "e40946e79bd24847f01d",
+    "url": "js/chunk-vendors.e3757cb5.js"
+  },
+  {
+    "revision": "3fa7348f187dae70fad03ee4cc4e0a4f",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
